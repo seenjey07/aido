34f4bcde69dd01a7006e0406297154852008ae0b
Christine Rosales <chrosales07@gmail.com>
2024-11-18 19:23:46 +0800

    Setup aido project.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Header</h1>
+        <h1>Chat</h1>
+        <h1>To-Do</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
